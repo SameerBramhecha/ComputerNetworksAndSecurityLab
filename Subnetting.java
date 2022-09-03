e4698e4373a56311a3a0d477d0c92b92cd35cfd3
Sameer Manoj Bramhecha <100222138+SameerBramhecha@users.noreply.github.com>
2022-09-03 17:21:33 +0530

    Add files via upload

---
+++ Subnetting.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Subnetting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter IP Address: ");
+        String ip = sc.next();
+        String ipad[] = ip.split("\\.");
+        int cc = Integer.parseInt(ipad[0]);
+        String mask = "";
+        if (cc >= 0 && cc <= 127) {
+            mask = "255.0.0.0";
+            System.out.println("Class A IP Address");
+        } else if (cc >= 128 && cc <= 191) {
+            System.out.println("Class B IP Address");
+            mask = "255.255.0.0";
+        } else if (cc >= 192 && cc <= 223) {
+            System.out.println("Class C IP Address");
+            mask = "255.255.255.0";
+        } else if (cc >= 224 && cc <= 239) {
+            System.out.println("Class D IP Address, used for Multcast");
+            mask = "255.0.0.0";
+        } else if (cc >= 240 && cc <= 254) {
+            System.out.println("Class E IP Address used for Experimental Use");
+            mask = "255.0.0.0";
+        } else {
+            System.out.println("Invalid IP Address");
+            System.exit(0);
+        }
+        String networkaddr = "";
+        String lastaddr = "";
+        String[] ipaddr = ip.split("\\.");
+        String msk[] = mask.split("\\.");
+        for (int i = 0; i < 4; i++) {
+            int x = Integer.parseInt(ipaddr[i]);
+            int y = Integer.parseInt(msk[i]);
+            int z = x & y;
+            networkaddr += z + ".";
+            int w = z | (y ^ 255);
+            lastaddr += w + ".";
+        }
+        System.out.println("First IP of the block:\n " + networkaddr);
+        System.out.println("Last IP of the block, i.e. BroadCast Address:\n " + lastaddr);
+
+        sc.close();
+    }
+
+}
